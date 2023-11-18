3e0fd7d34cb9b19a48ec0b88d3b175e63818e78f
sayudhalw <mazaya.studio@outlook.com>
2023-11-18 10:32:54 +0700

    Fix header menu display issue

--- app/public/wp-content/themes/fictionaluniversity/header.php
+++ app/public/wp-content/themes/fictionaluniversity/header.php
@@ -18,12 +18,12 @@
 			<div class="site-header__menu group">
 				<nav class="main-navigation">
 					<!-- <?php
-						wp_nav_menu(array(
-							'theme_location' => 'headerMenuLocation'
-						));
-					?> -->
+wp_nav_menu(array(
+	'theme_location' => 'headerMenuLocation'
+));
+?> -->
 					<ul>
-						<li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+						<li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 6) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
 						<li><a href="#">Programs</a></li>
 						<li><a href="#">Events</a></li>
 						<li><a href="#">Campuses</a></li>
