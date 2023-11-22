b0dfac356f57e853115db99b6e61ccecf0d4dbe6
sayudhalw <mazaya.studio@outlook.com>
2023-11-22 20:56:37 +0700

    Displaying Custom Post Types + Add event archive page and single event template

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,8 @@
 function university_post_types()
 {
 	register_post_type('event', array(
+		'has_archive' => true,
+		'rewrite' => array('slug' => 'events'),
 		'public' => true,
 		'show_in_rest' => true,
 		'labels' => array(
