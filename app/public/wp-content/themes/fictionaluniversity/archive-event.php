135f5b9af789c05d8ccdb0998a710131587d8e2f
sayudhalw <mazaya.studio@outlook.com>
2023-11-29 09:08:07 +0700

    Manipulating Default URL Based Queries [*] Update Queries [*] Update language files in advanced-custom-fields plugin

--- app/public/wp-content/themes/fictionaluniversity/archive-event.php
+++ app/public/wp-content/themes/fictionaluniversity/archive-event.php
@@ -11,12 +11,21 @@
 </div>
 
 <div class="container container--narrow page-section">
-	<?php while (have_posts()) :
-    the_post(); ?>
+	<?php
+		while (have_posts()):
+			the_post();
+	?>
 		<div class="event-summary">
-			<a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-				<span class="event-summary__month"><?php the_time('M'); ?></span>
-				<span class="event-summary__day"><?php the_time('d') ?></span>
+		<a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+				<span class="event-summary__month">
+					<?php
+					$eventDate = new DateTime(get_field('event_date'));
+					echo $eventDate->format('M');
+							?>
+				</span>
+				<span class="event-summary__day">
+					<?php echo $eventDate->format('d'); ?>
+				</span>
 			</a>
 			<div class="event-summary__content">
 				<h5 class="event-summary__title headline headline--tiny">
@@ -27,9 +36,6 @@
 				</p>
 			</div>
 		</div>
-	<?php
-  endwhile;
-  echo paginate_links();
-    ?>
+	<?php endwhile; ?>
 </div>
 <?php get_footer(); ?>
